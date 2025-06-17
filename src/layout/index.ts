822aad3c9d62e60e1aff0a83c2ea116d9ac609fe
Zonda6996 <ifresh3d@gmail.com>
2025-06-17 18:19:15 +0500

    Add common folder for background images

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,3 +1,2 @@
 export { Header } from './Header/Header'
-export { HeroSection } from './HeroSection/HeroSection'
-export { ServicesSection } from './ServicesSection/ServicesSection'
+export * from './common'
