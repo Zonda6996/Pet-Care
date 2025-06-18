6f388fa982fa257016fd47df26b5c4b2cc1c4a08
Zonda6996 <ifresh3d@gmail.com>
2025-06-18 15:09:59 +0500

    Add container style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
 	return (
-		<div className='relative min-h-screen'>
+		<div className='relative min-h-screen bg-light-gray-bg z-0'>
 			<ImagesBackground />
 			<Header />
-			<main>
+			<main className='max-w-[1380px] mx-auto flex flex-col'>
 				<Outlet />
 			</main>
 		</div>
