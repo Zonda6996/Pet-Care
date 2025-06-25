0856f09ff5f816beafaddae18537cec7559b47ae
Zonda6996 <ifresh3d@gmail.com>
2025-06-25 14:20:25 +0500

    Add animal category route

--- src/shared/routes/routes.ts
+++ src/shared/routes/routes.ts
@@ -6,14 +6,20 @@ export const ROUTES = {
 	REGISTER: '/register',
 	DASHBOARD: '/dashboard',
 	RESET_PASSWORD: '/reset-password',
+	CHOOSE_ANIMAL: '/choose-animal',
+	CHOOSE_ANIMAL_CATEGORY: '/choose-animal/:category',
 } as const
 
-// export type PathParams = {}
+export type PathParams = {
+	[ROUTES.CHOOSE_ANIMAL_CATEGORY]: {
+		category: string
+	}
+}
 
-// declare module 'react-router-dom' {
-// 	interface Register {
-// 		params: PathParams
-// 	}
-// }
+declare module 'react-router-dom' {
+	interface Register {
+		params: PathParams
+	}
+}
 
 export type RouteValue = (typeof ROUTES)[keyof typeof ROUTES]
