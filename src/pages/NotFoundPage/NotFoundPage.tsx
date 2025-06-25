2c5d236b0f68011bf357ff6e6804e2be35bfe8b1
Zonda6996 <ifresh3d@gmail.com>
2025-06-25 14:18:37 +0500

    Add Not found page

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,26 @@
+import { ROUTES } from '@/shared/routes/routes'
+import { Button } from '@/shared/ui/Button'
+import { ArrowRightIcon } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+function NotFoundPage() {
+	return (
+		<div className='pt-[140px] text-center space-y-5'>
+			<div className='flex flex-col gap-3'>
+				<span className='font-extrabold text-9xl'>404</span>
+				<p className='font-bold text-2xl'>Что-то пошло не так...</p>
+				<p className='font-light text-xl'>
+					Страница отсутствует, или вы перешли по неккоректному адресу.
+				</p>
+			</div>
+			<Link className='text-primary font-light text-xl' to={ROUTES.HOME}>
+				<Button>
+					На главную
+					<ArrowRightIcon className='mt-0.5' />
+				</Button>
+			</Link>
+		</div>
+	)
+}
+
+export const Component = NotFoundPage
