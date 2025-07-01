2cd2e90ae12984f2d86d7e38f8671e86b2605e4e
Zonda6996 <ifresh3d@gmail.com>
2025-07-01 09:49:09 +0500

    Added pet reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authReducer, authApi } from '@/modules/Auth'
+import { petReducer } from '@/modules/PetForm'
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		pet: petReducer,
 		[authApi.reducerPath]: authApi.reducer,
 	},
 	middleware: getDefaultMiddleware =>
